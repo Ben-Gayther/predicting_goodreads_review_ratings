b81d30203cec996604862a6522f1e2db3ddaf1f3
Ben Gayther <benjamin.gayther.15@ucl.ac.uk>
2023-03-15 17:33:25 +0000

    Use f1 to chose best model.

--- src/train_model.py
+++ src/train_model.py
@@ -114,7 +114,7 @@ def main():
         evaluation_strategy='epoch',
         save_strategy='epoch',
         load_best_model_at_end=True,
-        metric_for_best_model='accuracy',
+        metric_for_best_model='f1',
         greater_is_better=True,
         run_name='goodreads'
     )
