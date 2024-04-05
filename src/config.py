147fe2dcd1ee224d9f63add4d2ebf7acd4623903
Ben Gayther <benjigayther@hotmail.com>
2024-04-05 19:43:53 +0100

    Declare types in config file

--- src/config.py
+++ src/config.py
@@ -7,28 +7,28 @@ LOGGING = config.get("LOGGING", {})
 TRAINING_ARGS = config.get("TRAINING_ARGS", {})
 DATA_ARGS = config.get("DATA_ARGS", {})
 
-logging_level = LOGGING.get("LEVEL", "INFO")
-logging_format = LOGGING.get(
+logging_level: str = LOGGING.get("LEVEL", "INFO")
+logging_format: str = LOGGING.get(
     "FORMAT", "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 )
 
-model_name = TRAINING_ARGS.get(
+model_name: str = TRAINING_ARGS.get(
     "MODEL_NAME", "nlptown/bert-base-multilingual-uncased-sentiment"
 )
-learning_rate = TRAINING_ARGS.get("LEARNING_RATE", 2.0e-5)
-max_length = TRAINING_ARGS.get("MAX_LENGTH", 128)
-batch_size = TRAINING_ARGS.get("BATCH_SIZE", 1)
-epochs = TRAINING_ARGS.get("EPOCHS", 1)
-full_dataset = TRAINING_ARGS.get("FULL_DATASET", False)
-test_run = TRAINING_ARGS.get("TEST_RUN", True)
-output_dir = TRAINING_ARGS.get("OUTPUT_DIR", "models/")
-submission_name = TRAINING_ARGS.get("SUBMISSION_NAME", "submission.csv")
+learning_rate: float = TRAINING_ARGS.get("LEARNING_RATE", 2.0e-5)
+max_length: int = TRAINING_ARGS.get("MAX_LENGTH", 128)
+batch_size: int = TRAINING_ARGS.get("BATCH_SIZE", 1)
+epochs: int = TRAINING_ARGS.get("EPOCHS", 1)
+full_dataset: bool = TRAINING_ARGS.get("FULL_DATASET", False)
+test_run: bool = TRAINING_ARGS.get("TEST_RUN", True)
+output_dir: str = TRAINING_ARGS.get("OUTPUT_DIR", "models/")
+submission_name: str = TRAINING_ARGS.get("SUBMISSION_NAME", "submission.csv")
 
-input_train_data = DATA_ARGS.get("INPUT_TRAIN_DATA", "data/goodreads_train.csv")
-input_test_data = DATA_ARGS.get("INPUT_TEST_DATA", "data/goodreads_test.csv")
-output_train_data = DATA_ARGS.get(
+input_train_data: str = DATA_ARGS.get("INPUT_TRAIN_DATA", "data/goodreads_train.csv")
+input_test_data: str = DATA_ARGS.get("INPUT_TEST_DATA", "data/goodreads_test.csv")
+output_train_data: str = DATA_ARGS.get(
     "OUTPUT_TRAIN_DATA", "data/processed_goodreads_train.csv"
 )
-output_test_data = DATA_ARGS.get(
+output_test_data: str = DATA_ARGS.get(
     "OUTPUT_TEST_DATA", "data/processed_goodreads_test.csv"
 )
